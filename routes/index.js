aa09e3a8a7a32752146408c8d4e2483e9440a6fb
Spyros Argalias <sargalias@gmail.com>
2018-04-11 21:52:03 +0100

    complete filesize analysing functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
-const multer = require('multer');
+const path = require('path');
+const multer  = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage, limits: {fileSize: 1024*10}}).single('file');
 
 
-router.get('/get-file-size', function(req, res, next) {
-    res.json({size: 1280});
+router.post('/get-file-size', function(req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            if (err.code === "LIMIT_FILE_SIZE") {
+                res.json({err: 'The file is too large.'});
+                return;
+            }
+            res.json({err: err});
+            return;
+        }
+        res.send({size: req.file.size});
+    })
 });
 
 module.exports = router;
